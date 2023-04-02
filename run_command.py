889c5f617174f7957a6849d9d8c9f97c09f89103
Jim Newpower <jimnewpower@gmail.com>
2023-04-02 09:27:04 -0600

    Use os.listdir() instead of os.walk()

--- run_command.py
+++ run_command.py
@@ -1,22 +1,26 @@
-import subprocess
+import array
 import os
+import subprocess
 
 def run_command_in_subdirectories_one_level(command):
-    print(f'command:{command}')
-    print(f'cwd:{os.getcwd()}')
-    for path, dirs, files in os.walk('.'):
-        print(f'path {path}')
-        print(f'dirs {dirs}')
-        for d in dirs:
-            print(f'')
-            print(f'{d}')
-            print(f'Running {command} in {d}')
-            os.chdir(d)
-            try:
-                subprocess.run(command, shell=True, check=True)
-            except Exception:
-                print(f'Error')
-            os.chdir('..')
+    print(f'cwd : {os.getcwd()}')
+    print(f'command : {command}')
+    root = os.getcwd()
+    dirs = list()
+    for file in os.listdir(root):
+        if (os.path.isdir(os.path.join(root, file))):
+            dirs.append(file)
+
+    dirs.sort()
+    for subdir in dirs:
+        print(f'\n========================================')
+        print(f'{subdir} : {command}')
+        os.chdir(subdir)
+        try:
+            subprocess.run(command, shell=True, check=True)
+        except Exception:
+            print(f'Error')
+        os.chdir('..')
 
 command = input("Enter the shell command to run: ")
 run_command_in_subdirectories_one_level(command)
